7c805ab8aa52d6b20332576c21e1be187765509c
opuopu <nurmdopu428@gmail.com>
2025-01-19 16:57:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const KEY =
+  "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";
+
+const client = new OpenAI({
+  apiKey: KEY, // This is the default and can be omitted
+});
